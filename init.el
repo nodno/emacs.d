ac19b2cacbb0b740c8e14ba804954fec7dc1e5f8
zhaoweipu <zhaoweipu@gmail.com>
2019-09-14 17:22:29 +0800

    open the gete of helm, light up anaconda-mode

--- init.el
+++ init.el
@@ -11,6 +11,7 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+;;(load "~/.emacs.d/myinit.el" t)
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 
 (custom-set-variables
@@ -29,7 +30,7 @@
  '(frame-background-mode (quote dark))
  '(package-selected-packages
    (quote
-    (winner-mode wsd-mode ox-jira ox-md ox-twbs pos-tip popup showtip solarized-theme php-mode mu4e-alert org-pdfview nginx-mode smart-mode-line lua-mode lua git-gutter deft diminish moe-theme zenburn-theme yasnippet-snippets which-key use-package undo-tree try poet-theme org-bullets magit leuven-theme iedit hungry-delete htmlize flycheck-color-mode-line expand-region elpy doom-modeline counsel company-jedi color-theme-sanityinc-tomorrow color-theme-modern beacon ace-window)))
+    (helm-descbinds helm-projectile projectile helm-ls-git helm winner-mode wsd-mode ox-jira ox-md ox-twbs pos-tip popup showtip solarized-theme php-mode mu4e-alert org-pdfview nginx-mode smart-mode-line lua-mode lua git-gutter deft diminish moe-theme zenburn-theme yasnippet-snippets which-key use-package undo-tree try poet-theme org-bullets magit leuven-theme iedit hungry-delete htmlize flycheck-color-mode-line expand-region elpy doom-modeline counsel company-jedi color-theme-sanityinc-tomorrow color-theme-modern beacon ace-window)))
  '(tramp-shell-prompt-pattern
    "\\(?:^\\|\\)[^]#$%>
 ]*#?[]#$%>].* *\\(\\[[0-9;]*[a-zA-Z] *\\)*"))
@@ -38,4 +39,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
+ '(helm-selection ((t (:background "systemPurpleColor" :foreground "white")))))
